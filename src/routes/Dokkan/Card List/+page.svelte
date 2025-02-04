d29291741bc52648f6b225bc84813f9671a87f4b
lintonssj4 <lintonssj4@gmail.com>
2025-02-03 21:37:00 -0400

    Add files via upload

---
+++ src/routes/Dokkan/Card List/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import CardsDokkan from '$lib/components/CardsDokkan.svelte';
+    import {dokkancards} from '$lib/data.js';
+</script>
+
+<div class="row g-4">
+    {#each dokkancards as card}
+      <div class="col">
+        <CardsDokkan {...card} />
+      </div>
+    {/each}
+  </div>
---
+++ src/routes/Dokkan/Card List/Goku SSJ/+page.svelte
@@ -0,0 +1,107 @@
+<div class="body" style="background-color: #28ca1b;">
+    <div class="title">
+        <center><h1><b style="color: black;">Radiância incomparável</b></h1>
+        <h1><b style="color: black;">Goku Super Saiyajin + Vegeta Super Saiyajin(Anjo)</b></h1></center>
+    </div>
+    <div class="imagem">
+        <!-- svelte-ignore a11y_missing_attribute -->
+        <img src="https://pbs.twimg.com/media/GirCwSmW4AATvNX?format=jpg&name=4096x4096"  width="30%" height="80%">
+        <button id="swapButton" style="padding: 10px 20px; font-size: 16px; cursor: pointer; border: none; background-color: #007bff; color: white; border-radius: 5px; transition: background 0.3s;"><a href="/Dokkan/Card List/Goku SSJ/Vegeta SSJ">🔄</a></button>        
+    </div>
+    <center><a href="/Dokkan/Card List/Goku SSJ/Vegeta SSJ/Vegito SSJ" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" style="background-color: #155c0d;"><b>Fusão Especial!</b></a></center><br>   
+    <div class="detalhes">
+        <h3><b>Habilidade de Lider:</b><br>Personagens da categoria <b> Heróis Protetores da Terra </b>, <b>Guerreiros Fundidos</b> ou <b>Saiyajins Puros</b> ganham Ki +3, HP +200% ATK e DEF +170%; mais um adicional de HP, ATK e DEF +50% para personagens que também pertencem à categoria <b>Guerreiros Protetores da Terra</b> ou <b>Potara</b></h3>
+        <h3><b>Super Ataque:<br>- Kamehameha:</b> Aumenta o Ataque em +30%, Defesa em 50% por 1 turno e causa dano colossal a um inimigo</h3>
+        <h3><b>Ultra Super Ataque:<br>- Indo com tudo com o Super Saiyajin 3:</b> Aumenta o Ataque em 50%, Defesa em 30% por 1 turno e causa dano mega colossal a um inimigo</h3>
+        <h3><b>Passiva: O Plano do Ancião Kai<br>Efeito básicos:</b><br>
+            - Ataque e Defesa +200%;<br>
+            - Recebe um Ki adicional +2 por Tipo de Esfera de Ki obtido;<br>
+            <b>Para os aliados da categoria "Heróis Protetores da Terre" ou "Potara":</b><br>
+            - +1 de KI e +40% de Defesa;<br>
+            <b>Pelos primeiros 3 turnos quando o personagem entra na batalha:</b><br>
+            - Reduz o dano recebido em 50%;<br>
+            <b>Ao atacar com 10 ou mais de KI:</b><br>
+            - Ataque Efetivo contra todos os tipos;<br>
+            <b>Ao atacar com 20 ou mais de KI:</b><br>
+            - Lança um Super Ataque adicional;<br>
+            <b>Ao atacar no 2° ou 3° slot de ataque:</b><br>
+            - Reduz o dano recebido em 20%;<br>
+            - Lança um ataque que tem 70% de chance de se tornar um Super Ataque;<br>
+            <b>A partir do 4° turno da batalha:</b><br>
+            - Se encontre com Vegeta Super Saiyajin (anjo) e pode realizar a troca Reversíel!</h3>
+        <h3><b>Habilidade Ativa:<br>- Fusão especial:</b> Faça a fusão especial por 4 turnos.<br>
+        <b>Condição:</b> Pode ser ativada a partir do 5º turno do início da batalha (apenas uma vez, seja antes ou depois da Troca Reversível).</h3>
+    </div>
+    <h3><b>Habilidade de Ligação:<br>
+    - Super Saiyan: (Nível 10)</b> +15% de Ataque<br> 
+    <b>- Kamehameha: (Nível 10)</b> +10% de Ataque quando estiver performando um Super Ataque<br> 
+    <b>- Preparado para a batalha: (Nível 10)</b> +2 de KI e +5% de Ataque e Defesa<br> 
+    <b>- Tudo em Familia: (Nível 10)</b> +20% de Defesa<br>  
+    <b>- Lutadores Experientes: (Nível 10)</b> +15% de Ataque<br> 
+    <b>- Batalha Feroz: (Nível 10)</b> +20% de Ataque<br> 
+    <b>- Poder Lendário: (Nível 10)</b> +20% de Ataque quando estiver performando um Super Ataque</h3>
+    <h3><b>Categorias:<br>
+    - Guerreiros Ressuscitados<br> 
+    - Saga Majin Buu<br> 
+    - Potara<br> 
+    - Saiyajins Puros<br> 
+    - Família de Goku<br> 
+    - Família de Vegeta<br> 
+    - Super Saiyajins<br> 
+    - Rivais Dignos<br> 
+    - Kamehameha<br> 
+    - Guerreiros do Outro Mundo<br> 
+    - Carta de Trunfo Final<br> 
+    - Batalha de Inteligência<br> 
+    - Escola de Tartarugas<br> 
+    - Guerreiros Talentosos<br> 
+    - Limite de Tempo<br> 
+    - Evolução Dominada<br> 
+    - Vínculo de Amizade<br> 
+    - Batalha Acelerada<br> 
+    - Lutadores Fundidos<br> 
+    - Vínculo de Pais e Filhos<br> 
+    - Lutadores Criados na Terra <br>
+    - Heróis Protetores da Terra<br> 
+    - Sucessores</b></h3>
+
+</div>
+<style>
+    .body{
+        width: 98.9vw;
+        height: 400vh;
+
+    }
+
+h3 {
+    border: 3px solid #28ca1b; 
+    padding: 15px;
+    border-radius: 10px;
+    background-color: #1a1a1a;
+    color: white;
+    box-shadow: 0px 0px 10px #1fa628;
+    margin-bottom: 15px;
+}
+
+b {
+    color: #28ca1b;
+}
+.imagem{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px auto;
+}
+
+img {
+    border: 5px solid #1fa628;
+    border-radius: 15px;
+    box-shadow: 0px 0px 15px #1fa628;
+    max-width: 100%;
+    height: auto;
+}
+
+img:hover {
+    box-shadow: 0px 0px 25px #1fa628;
+}
+</style>
