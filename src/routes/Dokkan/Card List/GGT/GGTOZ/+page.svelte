3793f2bc65de4664700aec2455c860c5e56a5968
lintonssj4 <lintonssj4@gmail.com>
2025-02-03 20:24:04 -0400

    first commit

---
+++ src/routes/Dokkan/Card List/GGT/GGTOZ/+page.svelte
@@ -0,0 +1,86 @@
+<div class="body" style="background-color: #3ed4f5;">
+    <div class="title">
+        <center><h1><b style="color: black;">Poder Supremo de um Rugido Saiyajin</b></h1>
+        <h1><b style="color: black;">Goku (GT) Super Saiyajin 3 (Macaco Gigante Dourado)</b></h1>
+    </div>
+    <div class="imagem">
+        <!-- svelte-ignore a11y_missing_attribute -->
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqgjJ0OJsYFtSC9l9Og16nOszB6pUGdo_i2Q&s" width="30%" height="80%">
+    </div>
+<center><a href="/Dokkan/Card List/GGT/GGTOZ/GGTSSJ4" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" style="background-color: #146c8c;"><b>Pós Macaco Gigante</b></a></center><br>
+<center><a href="/Dokkan/Card List/GGT" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" style="background-color: #146c8c;"><b>Forma Base</b></a></center><br>
+    <div class="detalhes">
+        <h3><b>Habilidade de Lider:</b><br>Personagens da categoria <b>Poder do Macaco Gigante</b> ganham Ki +4 e HP, ATK e DEF +220%;</h3>
+        <h3><b>Ultra Super Ataque:<br>- Super Canhão de Chamas:</b> Aumenta o Ataque em 100% por 1 turno e causa dano destrutivo ao inimigo</h3>
+        <h3><b>Passiva: A fúria do macaco gigante dourado!</b><br>
+            <b>Efeitos Básicos:</b><br>
+            - Ataque +300%;<br>
+            - Realiza um acerto crítico;<br>
+            - +2 de Ki por cada orb coletada;<br>
+            <b>Quando a transformação de Macaco Gigante acaba:</b><br>
+            - Se transforma no Goku Super Saiyajin 4.</h3>
+        <h3><b>Condição de transformação:</b> Transforma-se no Goku Super Saiyajin 4 quando a Transformação do Macaco Gigante termina.</h3>
+    </div>
+            <h3><b>Habilidade de Ligação:<br></h3>  
+<h3><b>Categorias:<br>
+  - Super Saiyan 3<br>
+  - Forma Gigante<br> 
+  - Saiyajins Puros<br> 
+  - Impulso de Transformação<br> 
+  - Família de Goku<br> 
+  - Kamehameha<br> 
+  - Luta Total<br> 
+  - Poder do Macaco Gigante<br> 
+  - Escola de Tartarugas<br> 
+  - Despertar Milagroso<br> 
+  - Heróis GT<br> 
+  - Vínculo de Amizade<br> 
+  - Batalha Acelerada<br> 
+  - Vontade Confiada<br> 
+  - Batalha do Destino<br> 
+  - Poder Além do Super Saiyan<br> 
+  - Vínculo de Pais e Filhos<br>
+  - Lutadores Criados na Terra<br> 
+  - Poder dos Desejos<br>
+  - Poder Incontrolável<br>
+  - Heróis Protetores da Terra</h3>
+</div>
+<style>
+    .body{
+        width: 98.8vw;
+        height: 400vh;
+
+    }
+
+h3 {
+    border: 3px solid #29a8d6;
+    padding: 15px;
+    border-radius: 10px;
+    background-color: #1a1a1a;
+    color: white;
+    box-shadow: 0px 0px 10px #146c8c;
+    margin-bottom: 15px;
+}
+
+b {
+    color: #29a8d6;
+}
+.imagem{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px auto;
+}
+
+img {
+    border: 5px solid #146c8c;
+    border-radius: 30px;
+    box-shadow: 0px 0px 30px #146c8c;
+    max-width: 100%;
+    height: auto;
+}
+
+img:hover {
+    box-shadow: 0px 0px 50px #146c8c;
+}
+</style>
---
+++ src/routes/Dokkan/Card List/Gogeta SSJ4/+page.svelte
@@ -0,0 +1,92 @@
+<div class="body" style="background-color: #fad81d;">
+    <div class="title">
+        <center><h1><b style="color: black;">Guerreiros Saiyajins com Poder Supremo</b></h1>
+        <h1><b style="color: black;">Gogeta Super Saiyajin 4</b></h1></center>
+    </div>
+    <div class="imagem">
+        <!-- svelte-ignore a11y_missing_attribute -->
+        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7c789a6c-dc0f-4c90-9977-471e89ef548d/dezmcyv-e0b89716-30c9-41ef-a9fc-df165506da43.jpg/v1/fit/w_828,h_1104,q_70,strp/ssj4_gogeta_hd_by_clannadan_dezmcyv-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTcwNyIsInBhdGgiOiJcL2ZcLzdjNzg5YTZjLWRjMGYtNGM5MC05OTc3LTQ3MWU4OWVmNTQ4ZFwvZGV6bWN5di1lMGI4OTcxNi0zMGM5LTQxZWYtYTlmYy1kZjE2NTUwNmRhNDMuanBnIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.PWiLD5p02-enuWhyJkK938edcYPqAJpwoQrXwXKyZn0" width="30%" height="80%">
+    </div>
+    <div class="detalhes">
+        <h3><b>Habilidade de Lider:</b><br>Personagens da categoria <b> Guerreiros Fundidos </b> ou <b>Heróis GT</b> ganham Ki +3 e HP, ATK e DEF +170%; mais um adicional de HP, ATK e DEF +30% para personagens que também pertencem à categoria <b>Kamehameha</b></h3>
+        <h3><b>Super Ataque:<br>- Clone Kamehameha:</b> Aumenta o Ataque e Defesa em 50% por 1 turno e causa dano colossal a um inimigo</h3>
+        <h3><b>Ultra Super Ataque:<br>- Big Bang Kamehameha:</b> Aumenta o Ataque e Defesa em 100% por 1 turno, causa dano mega colossal a um inimigo e desabilita uma ação do inimigo</h3>
+        <h3><b>Passiva: O Super Gogeta chegou!<br>Efeito básicos:</b><br>
+            - Ataque e Defesa em 177%;<br>
+            - Recebe um Ki adicional +2 por Tipo de Esfera de Ki obtido;<br>
+            - Os ataques são eficazes contra todos os Tipos;<br>
+            - 30% de chance de realizar um acerto crítico;<br>
+            - 70% de chance de lançar um Super Ataque adicional;<br>
+            <b>Para cada esfera de ki arco-íris coletada:</b><br>
+            - Chance de realizar um acerto crítico +7%;<br>
+            - Taxa de redução de dano +7%;<br>
+            <b>Ao receber um Super Ataque:</b><br>
+            - Grande chance de anulá-lo e contra-atacar com um poder tremendo.</h3>
+            <h3><b>Habilidade Ativa:<br>- 100x Big Bang Kamehameha:</b> Causa um dano extremo a um inimigo e, dentro do turno de ativação, todos os ataques se tornam críticos<br>
+                <b>Condição:</b> Quando o seu PV ou o do inimigo estiver em 50% ou menos ao enfrentar apenas um inimigo.</h3>    
+            </div>
+    <h3><b>Habilidade de Ligação:<br>
+    - Super Saiyan: (Nível 10)</b> +15% de Ataque<br> 
+    <b>- Kamehameha: (Nível 10)</b> +10% de Ataque quando estiver performando um Super Ataque<br> 
+    <b>- Velocidade Chocante: (Nível 10)</b> +2 de KI e 5% de Defesa<br> 
+    <b>- GT: (Nível 10)</b> +2 de KI e +10% de Ataque e Defesa<br> 
+    <b>- Rugido Saiyan: (Nível 10)</b> +25% de Ataque e +10% de Defesa<br> 
+    <b>- Batalha Feroz: (Nível 10)</b> +20% de Ataque<br> 
+    <b>- Poder Lendário: (Nível 10)</b> +20% de Ataque quando estiver performando um Super Ataque</h3>
+    <h3><b>Categorias:<br>
+- Fusão<br>
+- Saga do Dragão das Sombras<br> 
+- Saiyajins Puros<br> 
+- Forças Unidas<br> 
+- Kamehameha<br> 
+- Carta de Trunfo Final<br> 
+- Poder do Macaco Gigante<br> 
+- Heróis GT<br> 
+- Limite de Tempo<br> 
+- Batalha Acelerada<br>
+- Batalha do Destino<br> 
+- Poder Além do Super Saiyajin<br> 
+- Lutadores Fundidos<br> 
+- Vínculo de Pais e Filhos<br>
+- Heróis Protetores da Terra</b></h3>
+
+</div>
+<style>
+    .body{
+        width: 98.8vw;
+        height: 400vh;
+
+    }
+
+h3 {
+    border: 3px solid #FFD700; 
+    padding: 15px;
+    border-radius: 10px;
+    background-color: #1a1a1a;
+    color: white;
+    box-shadow: 0px 0px 10px rgba(255, 215, 0, 0.7);
+    margin-bottom: 15px;
+}
+
+b {
+    color: #FFD700;
+}
+.imagem{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px auto;
+}
+
+img {
+    border: 5px solid #d4c41c;
+    border-radius: 15px;
+    box-shadow: 0px 0px 15px #d4c41c;
+    max-width: 100%;
+    height: auto;
+}
+
+img:hover {
+    box-shadow: 0px 0px 25px #d4c41c;
+}
+</style>
