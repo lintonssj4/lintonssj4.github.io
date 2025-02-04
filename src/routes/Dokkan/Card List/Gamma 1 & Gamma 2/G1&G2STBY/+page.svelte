3545f275131bfa3f19a30b47517972b1101294e2
lintonssj4 <lintonssj4@gmail.com>
2025-02-03 21:42:14 -0400

    Add files via upload

---
+++ src/routes/Dokkan/Card List/Gamma 1 & Gamma 2/G1&G2STBY/+page.svelte
@@ -0,0 +1,96 @@
+<div class="body" style="background-color: #c33ef5;">
+    <div class="title">
+        <center><h1><b style="color: black;">Heróis Radiantemente Brilhantes</b></h1>
+        <h1><b style="color: black;">Gamma 1 & Gamma 2(Modo de Espera)</b></h1></center>
+    </div>
+    <div class="imagem">
+                <!-- svelte-ignore a11y_missing_attribute -->
+        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/61dfd049-e503-40f4-bc40-a686b8e06bcc/dgwf5o0-be76437d-6590-43fa-91c5-544179f15426.png/v1/fill/w_1280,h_1707,q_80,strp/int_lr_gamma1_and_gamma_2__standby__hd_art_by_kevmd11_dgwf5o0-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTcwNyIsInBhdGgiOiJcL2ZcLzYxZGZkMDQ5LWU1MDMtNDBmNC1iYzQwLWE2ODZiOGUwNmJjY1wvZGd3ZjVvMC1iZTc2NDM3ZC02NTkwLTQzZmEtOTFjNS01NDQxNzlmMTU0MjYucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.r2ewRibuZr1wI1mT1HA0tAsrX-jPUO8HZdRRZhXAsDc" width="30%" height="80%">
+    </div>
+    <center><a href="/Dokkan/Card List/Gamma 1 & Gamma 2/G1&G2STBY/Gamma 1" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" style="background-color: #7a18a8;"><b>Pós Modo de Espera</b></a></center><br>
+    <center><a href="/Dokkan/Card List/Gamma 1 & Gamma 2" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" style="background-color: #7a18a8;"><b>Forma Base</b></a></center><br>   
+    <div class="detalhes">
+        <h3><b>Habilidade de Lider:</b><br>Personagens da categoria <b>Super Heróis</b> ganham Ki +3 e HP, ATK e DEF +200%;</h3>
+        <h3><b>Super Ataque:<br>- Assaltp Heróico:</b> Aumenta o Ataque e a Defesa em 30% por 1 turno e causa dano colossal ao inimigo</h3>
+        <h3><b>Ultra Super Ataque:<br>- Super Explosão Gamma:</b> Aumenta o Ataque e a Defesa em 50% por 1 turno e causa dano mega-colossal ao inimigo, aumenta a Defesa dos aliados da classe "Super" em +50% por 1 turno</h3>
+        <h3><b>Passiva: Cada um com suas resoluções</b><br>
+            <b>Efeitos Básicos:</b><br>
+            - +6 de KI, Ataque e Defesa +250% e reduz o dano recebido em 50%;<br>
+            <b>Ao atacar:</b><br>
+            - Ataque e Defesa +50%;<br>
+            <b>Para aliados da categoria "Super Heróis", "Forças Unidas" ou "Defensores da Justiça":</b><br>
+            - +3 de KI, Ataque e Defesa +50%;<br>
+            <b>Para cada Orb "INT" coletada:</b><br>
+            - Lança um Super Ataque adicional(até no máximo dois);<br>
+            - Chance de performar um acerto crítico em +30%;<br>
+            <b>Para cada Orb "AGL" ou "STR" coletada:</b><br>
+            - 70% de chance de lançar um Super Ataque adicional(até no máximo dois);<br>
+            - Chance de performar um acerto crítico em +12%;<br></h3>
+        <h3><b>Habilidade Finalizadora:<br>Liberação total de energia:</b> Aumenta o ataque em +100% por cada carga acumulada, causa super-extremo no inimigo e troca de persongaem para o Gamma 1;<br> 
+            <b>Condição:</b> Pode ser ativado quando tiver 1 carga ou mais</h3>
+    </div>
+    <h3><b>Habilidade de Ligação:<br>
+        - Assalto Android: (Nível 10)</b> +2 de KI e +20% de Defesa<br> 
+        <b>- Exército RR: (Nível 10)</b> +10% de Ataque e Defesa<br> 
+        <b>- Herói da Justiça: (Nível 10)</b> +25% de Ataque, +5% de chance de acerto crítico<br> 
+        <b>- Velocidade Chocante: (Nível 10)</b> +2 de KI e 5% de Defesa<br> 
+        <b>- Pose de Assinatura: (Nível 10)</b> +3 de KI e 7% de Ataque<br>
+        <b>- Batalha Feroz: (Nível 10)</b> +20% de Ataque<br> 
+        <b>- Poder Lendário: (Nível 10)</b> +20% de Ataque quando estiver performando um Super Ataque</h3>  
+<h3><b>Categorias:<br>
+    - Forças Unidas<br>
+    - Androids<br> 
+    - Poder Total<br> 
+    - Batalha Acelerada<br> 
+    - Formas de VIdas Artifíciais<br> 
+    - Pose especial<br> 
+    - Esperança Conectada<br> 
+    - Defensores da Justiça<br> 
+    - Rivais Dignos<br> 
+    - Super Heróis<br> 
+    - Poder Total<br>
+    - Heróis de FIlme<br>
+    - Último Triunfo<br>
+    - Batalha do Destino<br>
+    - Vontade Confiada</h3>
+</div>
+<style>
+    .body{
+        width: 98.8vw;
+        height: 400vh;
+
+    }
+
+h3 {
+    border: 3px solid #c33ef5;
+    padding: 15px;
+    border-radius: 10px;
+    background-color: #1a1a1a;
+    color: white;
+    box-shadow: 0px 0px 10px #7a18a8;
+    margin-bottom: 15px;
+}
+
+b {
+    color: #c33ef5;
+}
+.imagem{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px auto;
+}
+
+img {
+    border: 5px solid #7a18a8;
+    border-radius: 15px;
+    box-shadow: 0px 0px 15px #7a18a8;
+    max-width: 100%;
+    height: auto;
+}
+
+img:hover {
+    box-shadow: 0px 0px 25px #7a18a8;
+}
+</style>
+
